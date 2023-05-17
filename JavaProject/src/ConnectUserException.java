24538f023eac46d5a3df2229d79dc263fc1b8e07
Nathan Therasse <72527627+SkyVeo@users.noreply.github.com>
2023-05-18 00:40:14 +0200

    Delete JavaProject directory

--- JavaProject/src/ConnectUserException.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class ConnectUserException extends Throwable{
-    private String string;
-    public ConnectUserException(String string) {
-        this.string = string;
-        JOptionPane.showMessageDialog(null, string, "Error", JOptionPane.ERROR_MESSAGE);
-    }
-}
