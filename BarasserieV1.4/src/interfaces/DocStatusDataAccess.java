905317ac936477dccbe9ea59f5675ce931bb92cd
Nathan Therasse <72527627+SkyVeo@users.noreply.github.com>
2023-05-18 00:39:34 +0200

    Delete BarasserieV1.4 directory

--- BarasserieV1.4/src/interfaces/DocStatusDataAccess.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-import java.sql.SQLException;
-import java.util.List;
-
-public interface DocStatusDataAccess {
-    public List<String> getAllDocStatus() throws SQLException;
-}
